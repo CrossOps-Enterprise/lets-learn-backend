1adaf51a76ba8f57153f1f7c16d12feb980564c2
Umair Ahmed <43206820+umairx97@users.noreply.github.com>
2020-03-15 17:32:49 +0500

    feat: fix autocatcher with middlewares (#19)

--- server/middlewares.js
+++ server/middlewares.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser').json({ extended: false })
 const cookieParser = require('cookie-parser')()
+const { STATUS_CODES } = require('http')
 const cors = require('cors')()
 const logger = require('../services/logger')
 
@@ -7,11 +8,26 @@ module.exports = {
   bodyParser,
   cookieParser,
   appLogger,
-  cors
+  cors,
+  handleError,
+  notFound
 }
 
 function appLogger (req, res, next) {
   req.appLogger = logger
   logger.info(`${req.hostname} ${req.path} ${req.method} ${req.header('user-agent')} `)
   next()
+}
+
+function handleError (err, req, res, next) {
+  if (res.headersSent) return next(err)
+
+  const statusCode = err.status || err.statusCode || err.code || 500
+  const errorMessage = STATUS_CODES[statusCode] || 'Internal Error'
+
+  res.status(statusCode).json({ error: errorMessage })
+}
+
+function notFound (req, res) {
+  res.status(404).json({ error: 'Not Found' })
 }
