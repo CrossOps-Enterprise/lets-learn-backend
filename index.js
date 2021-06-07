7f59d916f79e1aa5c0566ea67cd81e27a647f109
umairx97 <umairahmedbajwa97@gmail.com>
2021-06-07 06:34:58 +0500

    feat: add private package

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const PORT = process.env.PORT || 1337
 const { name, version } = require('./package.json')
 const chalk = require('chalk')
 const emoji = require('node-emoji')
+const greet = require('@crossops-enterprise/gh-package')
 
 require('./server')({ port: PORT }, () => {
+  greet()
   console.log(
     chalk.yellow.bold(`${emoji.get('rocket')} ${name} ${version} listening on port ${PORT}`)
   )
